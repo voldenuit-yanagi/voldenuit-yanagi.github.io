8fe9fa9337260cf661bbafbd94f3207e2ab88840
ahKevinXy <2502329620@qq.com>
2023-12-12 19:50:50 +0800

    添加主题

---
+++ themes/gallery/tailwind.config.js
@@ -0,0 +1,25 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./hugo_stats.json", "./layouts/**/*.html"],
+  darkMode: "class",
+  experimental: {
+    optimizeUniversalDefaults: true,
+  },
+  theme: {
+    extend: {
+      colors: {
+        neutral: {
+          850: "#1f1f1f",
+        },
+      },
+      fontFamily: {
+        sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        "3xl": "1792px",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
